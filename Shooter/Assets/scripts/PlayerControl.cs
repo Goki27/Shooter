ed29ec78cd51ede34de3bbaedf8ad6cc0d591f43
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2023-08-26 00:42:28 +0530

    implemented basic touch system
    implemented touch system using the new unity input system not full done added basic movement

---
+++ Shooter/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera mainCam;
+    private Vector3 offset;
+    void Start()
+    {
+        mainCam= Camera.main;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Touch.fingers[0].isActive)
+        {
+            Touch mytouch = Touch.activeTouches[0];
+            Vector3 touchPos = mytouch.screenPosition;
+            touchPos=mainCam.ScreenToWorldPoint(touchPos);
+
+            if (Touch.activeTouches[0].phase == TouchPhase.Began) { 
+            
+               offset = touchPos-transform.position;
+            
+            }
+            if (Touch.activeTouches[0].phase==TouchPhase.Moved) 
+            {
+
+                transform.position = new Vector3(touchPos.x-offset.x, touchPos.y-offset.y, 0);
+
+            }
+            if (Touch.activeTouches[0].phase==TouchPhase.Stationary) { 
+            
+            transform.position=new Vector3(touchPos.x-offset.x,touchPos.y-offset.y, 0);
+            
+            
+            }
+
+        }
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+}
